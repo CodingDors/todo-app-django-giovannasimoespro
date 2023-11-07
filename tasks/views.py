1fcec3cf827310adcccd8cbb9099c1abd36f1beb
giovannasimoespro <98493346+giovannasimoespro@users.noreply.github.com>
2023-11-07 22:45:18 +0000

    test

--- tasks/views.py
+++ tasks/views.py
@@ -4,5 +4,13 @@ tasks = []
 
 # Create your views here.
 def index(request):
-    # TODO
-    return render(request, "tasks/index.html")
+    if request.method == "GET":
+        return render(request, "tasks/index.html", {
+            "tasks": tasks
+        })
+    else:
+        new_task = request.POST["new_task"]
+        tasks.append(new_task)
+        return render(request, "tasks/index.html", {
+            "tasks": tasks
+        }) 
